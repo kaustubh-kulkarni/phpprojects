4ddc14ceb2880b75e8a3a79fae3c1f9f8778c497
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-02-22 16:17:18 +0100

    Added class_methods file to understand different methods in php

---
+++ class_methods.php
@@ -0,0 +1,27 @@
+<?php
+
+class Cars {
+
+//creating a function
+function greeting(){
+
+}
+
+function greeting2() {
+    
+}
+
+}
+
+//find if the method exists in class Cars
+$my_methods = get_class_methods('Cars');
+
+foreach ($my_methods as $method) {
+    echo $method . "<br>";
+}
+
+
+
+
+
+?>
